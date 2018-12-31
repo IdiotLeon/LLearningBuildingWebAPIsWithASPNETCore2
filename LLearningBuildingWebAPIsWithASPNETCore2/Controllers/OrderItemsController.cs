b39ca94f59d34fd7bc6fd00d1b284fb07859d617
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2018-12-31 11:43:14 -0800

    demo: Adding Controllers
    module 2.2

---
+++ LLearningBuildingWebAPIsWithASPNETCore2/Controllers/OrderItemsController.cs
@@ -0,0 +1,58 @@
+﻿using LLearningBuildingWebAPIsWithASPNETCore2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LLearningBuildingWebAPIsWithASPNETCore2.Controllers
+{
+    [Produces("application/json")]
+    [Route("")]
+    public class OrderItemsController : Controller
+    {
+        private readonly H_Plus_SportsContext _context;
+
+        public OrderItemsController(H_Plus_SportsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetOrderItem()
+        {
+            return new ObjectResult(_context.OrderItem);
+        }
+
+        [HttpGet("{id}", Name = "GetOrderItem")]
+        public async Task<IActionResult> GetOrderItem([FromRoute] int id)
+        {
+            var orderItem = await _context.OrderItem.SingleOrDefaultAsync(m => m.OrderItemId == id);
+            return Ok(orderItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderItem([FromRoute] int id, [FromBody] OrderItem orderItem)
+        {
+            _context.Entry(orderItem).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(orderItem);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostOrderItem([FromBody] OrderItem orderItem)
+        {
+            _context.OrderItem.Add(orderItem);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetOrderItem", new { id = orderItem.OrderItemId }, orderItem);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderItem([FromBody] int id)
+        {
+            var orderItem = await _context.OrderItem.SingleOrDefaultAsync(m => m.OrderItemId == id);
+            _context.OrderItem.Remove(orderItem);
+            await _context.SaveChangesAsync();
+            return Ok(orderItem);
+        }
+    }
+}
---
+++ LLearningBuildingWebAPIsWithASPNETCore2/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using LLearningBuildingWebAPIsWithASPNETCore2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LLearningBuildingWebAPIsWithASPNETCore2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/products")]
+    public class ProductsController : Controller
+    {
+        private readonly H_Plus_SportsContext _context;
+
+        public ProductsController(H_Plus_SportsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return new ObjectResult(_context.Product);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<IActionResult> GetProduct([FromRoute] string id)
+        {
+            var product = await _context.Product.SingleOrDefaultAsync(m => m.ProductId == id);
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct([FromRoute] string id, [FromBody] Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostProduct([FromBody] Product product)
+        {
+            _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] string id)
+        {
+            var product = await _context.Product.SingleOrDefaultAsync(m => m.ProductId == id);
+            await _context.SaveChangesAsync();
+            return Ok(product);
+        }
+    }
+}
